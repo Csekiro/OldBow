33479830c648c3dbc912a4749f798660c2a98581
Csekiro <csekiro@outlook.com>
2026-03-26 13:04:09 +0800

    cleararrows_mannequin

--- src/main/java/Csekiro/oldbow/command/ClearArrowsCommand.java
+++ src/main/java/Csekiro/oldbow/command/ClearArrowsCommand.java
@@ -4,9 +4,10 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 import java.util.Collection;
@@ -19,48 +20,67 @@ public final class ClearArrowsCommand {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
                 dispatcher.register(
                         CommandManager.literal("cleararrows")
-                                .then(CommandManager.argument("targets", EntityArgumentType.players())
+                                .then(CommandManager.argument("targets", EntityArgumentType.entities())
                                         .executes(ClearArrowsCommand::execute))
                 )
         );
     }
 
     private static int execute(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
-        Collection<ServerPlayerEntity> targets = EntityArgumentType.getPlayers(context, "targets");
+        Collection<? extends Entity> targets = EntityArgumentType.getEntities(context, "targets");
 
-        int affectedPlayers = 0;
+        int affectedEntities = 0;
         int removedArrows = 0;
+        int skippedEntities = 0;
 
-        for (ServerPlayerEntity player : targets) {
-            int arrowCount = player.getStuckArrowCount();
+        for (Entity entity : targets) {
+            if (!(entity instanceof LivingEntity livingEntity)) {
+                skippedEntities++;
+                continue;
+            }
+
+            int arrowCount = livingEntity.getStuckArrowCount();
             if (arrowCount > 0) {
-                player.setStuckArrowCount(0);
-                affectedPlayers++;
+                livingEntity.setStuckArrowCount(0);
+                affectedEntities++;
                 removedArrows += arrowCount;
             }
         }
 
         ServerCommandSource source = context.getSource();
 
-        if (affectedPlayers == 0) {
-            source.sendFeedback(() -> Text.literal("目标身上没有可清除的箭矢。"), false);
+        if (affectedEntities == 0) {
+            if (skippedEntities > 0) {
+                source.sendFeedback(() -> Text.literal("目标中没有可清除箭矢的活体实体。"), false);
+            } else {
+                source.sendFeedback(() -> Text.literal("目标身上没有可清除的箭矢。"), false);
+            }
             return 0;
         }
 
         if (targets.size() == 1) {
-            ServerPlayerEntity player = targets.iterator().next();
+            Entity target = targets.iterator().next();
             int finalRemovedArrows = removedArrows;
             source.sendFeedback(() -> Text.literal(
-                    "已清除 " + player.getName().getString() + " 身上的 " + finalRemovedArrows + " 支箭矢。"
+                    "已清除 " + target.getName().getString() + " 身上的 " + finalRemovedArrows + " 支箭矢。"
             ), false);
         } else {
-            int finalAffectedPlayers = affectedPlayers;
+            int finalAffectedEntities = affectedEntities;
             int finalRemovedArrows = removedArrows;
-            source.sendFeedback(() -> Text.literal(
-                    "已清除 " + finalAffectedPlayers + " 名玩家身上的箭矢，共 " + finalRemovedArrows + " 支。"
-            ), false);
+            int finalSkippedEntities = skippedEntities;
+
+            if (finalSkippedEntities > 0) {
+                source.sendFeedback(() -> Text.literal(
+                        "已清除 " + finalAffectedEntities + " 个活体目标身上的箭矢，共 " + finalRemovedArrows +
+                                " 支；跳过 " + finalSkippedEntities + " 个非活体实体。"
+                ), false);
+            } else {
+                source.sendFeedback(() -> Text.literal(
+                        "已清除 " + finalAffectedEntities + " 个目标身上的箭矢，共 " + finalRemovedArrows + " 支。"
+                ), false);
+            }
         }
 
-        return affectedPlayers;
+        return affectedEntities;
     }
 }
