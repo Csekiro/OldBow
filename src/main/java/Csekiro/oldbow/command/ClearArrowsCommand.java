8d0c57652d220bf6cb7e5592adf44df3981e7ca5
Csekiro <csekiro@outlook.com>
2026-03-26 12:51:06 +0800

    cleararrows

--- src/main/java/Csekiro/oldbow/command/ClearArrowsCommand.java
+++ src/main/java/Csekiro/oldbow/command/ClearArrowsCommand.java
@@ -1,12 +1,15 @@
 package Csekiro.oldbow.command;
 
-import com.mojang.brigadier.Command;
+import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
+
+import java.util.Collection;
 
 public final class ClearArrowsCommand {
     private ClearArrowsCommand() {
@@ -16,23 +19,48 @@ public final class ClearArrowsCommand {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
                 dispatcher.register(
                         CommandManager.literal("cleararrows")
-                                .requires(source -> source.getEntity() instanceof ServerPlayerEntity)
-                                .executes(context -> execute(context.getSource()))
+                                .then(CommandManager.argument("targets", EntityArgumentType.players())
+                                        .executes(ClearArrowsCommand::execute))
                 )
         );
     }
 
-    private static int execute(ServerCommandSource source) throws CommandSyntaxException {
-        ServerPlayerEntity player = source.getPlayerOrThrow();
-        int arrowCount = player.getStuckArrowCount();
+    private static int execute(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        Collection<ServerPlayerEntity> targets = EntityArgumentType.getPlayers(context, "targets");
 
-        if (arrowCount <= 0) {
-            source.sendFeedback(() -> Text.literal("你身上没有插着的箭矢。"), false);
-            return Command.SINGLE_SUCCESS;
+        int affectedPlayers = 0;
+        int removedArrows = 0;
+
+        for (ServerPlayerEntity player : targets) {
+            int arrowCount = player.getStuckArrowCount();
+            if (arrowCount > 0) {
+                player.setStuckArrowCount(0);
+                affectedPlayers++;
+                removedArrows += arrowCount;
+            }
         }
 
-        player.setStuckArrowCount(0);
-        source.sendFeedback(() -> Text.literal("已清除你身上的 " + arrowCount + " 支箭矢。"), false);
-        return Command.SINGLE_SUCCESS;
+        ServerCommandSource source = context.getSource();
+
+        if (affectedPlayers == 0) {
+            source.sendFeedback(() -> Text.literal("目标身上没有可清除的箭矢。"), false);
+            return 0;
+        }
+
+        if (targets.size() == 1) {
+            ServerPlayerEntity player = targets.iterator().next();
+            int finalRemovedArrows = removedArrows;
+            source.sendFeedback(() -> Text.literal(
+                    "已清除 " + player.getName().getString() + " 身上的 " + finalRemovedArrows + " 支箭矢。"
+            ), false);
+        } else {
+            int finalAffectedPlayers = affectedPlayers;
+            int finalRemovedArrows = removedArrows;
+            source.sendFeedback(() -> Text.literal(
+                    "已清除 " + finalAffectedPlayers + " 名玩家身上的箭矢，共 " + finalRemovedArrows + " 支。"
+            ), false);
+        }
+
+        return affectedPlayers;
     }
 }
